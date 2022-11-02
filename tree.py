12a8fbff6475bc5326c2d39fc44d3ddc4c53c4bf
Dima <Ddelariu@ya.ru>
2022-11-02 20:47:24 +0300

    Fix segment tree

--- tree.py
+++ tree.py
@@ -20,10 +20,12 @@ class BaseTree(abc.ABC):
 
 class Tree(BaseTree):
     peaks: list
+    add_peaks: list
 
     def __init__(self, array: list) -> None:
         super().__init__(array)
         self.peaks = [0] * (4 * self.length)
+        self.add_peaks = [0] * (4 * self.length)
         self.build(1, 0, self.length - 1)
 
     def build(self, v: int, tl: int, tr: int) -> None:
@@ -35,27 +37,30 @@ class Tree(BaseTree):
             self.build(2 * v + 1, tm + 1, tr)
             self.peaks[v] = self.peaks[2 * v] + self.peaks[2 * v + 1]
 
-    def sum(self, v: int, tl: int, tr: int, l: int, r: int) -> int:
+    def sum(self, v: int, tl: int, tr: int, l: int, r: int) -> (int, int):
         if l > r:
-            return 0
+            return 0, 0
         if l == tl and r == tr:
-            return self.peaks[v]
+            return self.peaks[v], tr - tl + 1
         tm: int = (tl + tr) // 2
-        return self.sum(2 * v, tl, tm, l, min(r, tm)) + self.sum(2 * v + 1, tm + 1, tr, max(l, tm + 1), r)
+        val1: (int, int) = self.sum(2 * v, tl, tm, l, min(r, tm))
+        val2: (int, int) = self.sum(2 * v + 1, tm + 1, tr, max(l, tm + 1), r)
+        return val1[0] + val2[0] + self.add_peaks[v] * (val1[1] + val2[1]), val1[1] + val2[1]
 
     def get_sum(self, l: int, r: int) -> int:
-        return self.sum(1, 0, self.length - 1, l, r)
+        return self.sum(1, 0, self.length - 1, l, r)[0]
 
     def update(self, v: int, tl: int, tr: int, l: int, r: int, add: int) -> None:
         if l > r:
             return None
         if l == tl and r == tr:
+            self.add_peaks[v] += add
             self.peaks[v] += add * (tr - tl + 1)
-        else:
-            tm: int = (tl + tr) // 2
-            self.update(2 * v, tl, tm, l, min(r, tm), add)
-            self.update(2 * v + 1, tm + 1, tr, max(l, tm + 1), r, add)
-            self.peaks[v] = self.peaks[2 * v] + self.peaks[2 * v + 1]
+            return None
+        tm: int = (tl + tr) // 2
+        self.update(2 * v, tl, tm, l, min(r, tm), add)
+        self.update(2 * v + 1, tm + 1, tr, max(l, tm + 1), r, add)
+        self.peaks[v] = self.peaks[2 * v] + self.peaks[2 * v + 1] + self.add_peaks[v] * (tr - tl + 1)
 
     def do_update(self, l: int, r: int, x) -> None:
         self.update(1, 0, self.length - 1, l, r, x)
