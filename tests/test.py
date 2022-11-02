6b66631f4acd87f632349ac89e250d6e6f40e4e1
Dima <Ddelariu@ya.ru>
2022-11-02 17:03:41 +0300

    Add yml

---
+++ tests/test.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import unittest
+from tree import Tree, TestTree, BaseTree
+from main import read_files
+
+
+sys.path.append(os.getcwd())
+
+
+def ans_class_tree(data, flag: bool) -> list[int]:
+    ans: list[int] = []
+    tec_free: BaseTree
+    if flag:
+        tec_tree = Tree(data[1])
+    else:
+        tec_tree = TestTree(data[1])
+    for i in range(data[2]):
+        tec: list = data[i + 3]
+        if tec[0] == 1:
+            tec_tree.do_update(tec[1], tec[2], tec[3])
+        else:
+            ans.append(tec_tree.get_sum(tec[1], tec[2]))
+    return ans
+
+
+class TestSegmentTree(unittest.TestCase):
+
+    def test_correct_ans(self):
+        for file in os.listdir('files'):
+            data: list = read_files(f'files/{file}')
+            ans1: list[int] = ans_class_tree(data, True)
+            ans2: list[int] = ans_class_tree(data, False)
+            self.assertEqual(len(ans1), len(ans2))
+            for i in range(len(ans1)):
+                self.assertEqual(ans1[i], ans2[i])
+
+
+if __name__ == '__main__':
+    unittest.main()
