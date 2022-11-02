8cf6fb88efc25f96b1db4dd4f7b6f7f7e8975b22
Dima <Ddelariu@ya.ru>
2022-11-02 15:54:35 +0300

    Update tree

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tree import Tree
+from tree import Tree, TestTree
 
 
 def read_files(file_name: str) -> list:
--- tree.py
+++ tree.py
@@ -1,11 +1,28 @@
-class Tree:
+import abc
+
+
+class BaseTree(abc.ABC):
     array: list
-    peaks: list
     length: int
 
     def __init__(self, array: list) -> None:
-        self.array = array
+        self.array = array.copy()
         self.length = len(self.array)
+
+    @abc.abstractmethod
+    def get_sum(self, l: int, r: int) -> int:
+        raise NotImplemented
+
+    @abc.abstractmethod
+    def do_update(self, l: int, r: int, add: int) -> None:
+        raise NotImplemented
+
+
+class Tree(BaseTree):
+    peaks: list
+
+    def __init__(self, array: list) -> None:
+        super().__init__(array)
         self.peaks = [0] * (4 * self.length)
         self.build(1, 0, self.length - 1)
 
@@ -42,3 +59,13 @@ class Tree:
 
     def do_update(self, l: int, r: int, x) -> None:
         self.update(1, 0, self.length - 1, l, r, x)
+
+
+class TestTree(BaseTree):
+
+    def get_sum(self, l: int, r: int) -> int:
+        return sum(self.array[l:r + 1])
+
+    def do_update(self, l: int, r: int, add: int) -> None:
+        for i in range(l, r + 1):
+            self.array[i] += add
